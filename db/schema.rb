edc9a639b3873c430f23aaee736835586aadb9c9
EC2 Default User <ec2-user@ip-172-31-20-186.us-east-2.compute.internal>
2020-07-01 15:55:56 +0000

    usersテーブルへカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_01_014631) do
+ActiveRecord::Schema.define(version: 2020_07_01_154714) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", null: false
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2020_07_01_014631) do
     t.string "salt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "number"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
