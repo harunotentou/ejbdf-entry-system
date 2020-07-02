8536e34981a25ec47cdc4d910d88a13059c932c6
EC2 Default User <ec2-user@ip-172-31-20-186.us-east-2.compute.internal>
2020-07-02 08:35:10 +0000

    ログイン機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root  'static_pages#home'
   resources :users
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
